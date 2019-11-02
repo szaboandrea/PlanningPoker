bc289c87e1eb920fe04149edab4c2db0ca39ac40
= <=>
2019-11-02 14:14:28 +0200

    First Attila commit

--- app/src/main/java/com/example/planningpoker/MainActivity.java
+++ app/src/main/java/com/example/planningpoker/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.planningpoker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_container);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/planningpoker/ScoringFragment.java
@@ -0,0 +1,101 @@
+package com.example.planningpoker;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ScoringFragment.OnScoringFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ScoringFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ScoringFragment extends Fragment {
+    private static final String TAG = "ScoringFragment";
+    private OnScoringFragmentInteractionListener mListener;
+
+    private RecyclerView mRecyclerViewScoreList;
+    private Button mButtonSubmit;
+
+    public ScoringFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ScoringFragment newInstance() {
+        ScoringFragment fragment = new ScoringFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = getView();
+        if (view == null) {
+            view = inflater.inflate(R.layout.fragment_scoring, container, false);
+        }
+
+        mRecyclerViewScoreList = view.findViewById(R.id.recyclerViewScoreList);
+        mButtonSubmit = view.findViewById(R.id.buttonSubmit);
+
+        mButtonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return view;
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnScoringFragmentInteractionListener) {
+            mListener = (OnScoringFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnScoringFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
