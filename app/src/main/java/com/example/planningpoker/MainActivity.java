ed3f704d19c84fe3546f359a60efb44fbaad8a69
szaboandrea <szaboandi003@gmail.com>
2019-11-02 19:56:49 +0200

    Login Page

--- app/src/main/java/com/example/planningpoker/MainActivity.java
+++ app/src/main/java/com/example/planningpoker/MainActivity.java
@@ -1,42 +1,18 @@
 package com.example.planningpoker;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.util.Log;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity implements
-        ScoringFragment.OnScoringFragmentInteractionListener {
-    private static final String TAG = "PlanningPokerMain";
-
-    private ScoringFragment mScoringFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "App starting");
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_container);
+        setContentView(R.layout.activity_main);
 
-        mScoringFragment = ScoringFragment.newInstance();
-
-        if (findViewById(R.id.fragmentContainer) != null) {
-            if (savedInstanceState != null) {
-                mScoringFragment = (ScoringFragment)
-                        getSupportFragmentManager().findFragmentByTag("scoring_fragment");
-                Log.d(TAG, "Fragment loaded.");
-            }
-            else{
-                getSupportFragmentManager().beginTransaction()
-                        .add(R.id.fragmentContainer, mScoringFragment, "scoring_fragment").commit();
-                Log.d(TAG, "Fragment added.");
-            }
-        }
-        else{
-            Log.d(TAG, "Fragment container not found.");
-        }
     }
 
-    public void replaceFragment(int fragment){
-    }
 }
